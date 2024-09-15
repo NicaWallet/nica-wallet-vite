2b4fa4b935fa13963c95a85546ba9c6ccd80e137
56ibarra89 <56valle89@gmail.com>
2024-09-15 17:07:32 -0600

    new translation

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,19 +1,20 @@
 import { Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container, LinearProgress, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import LanguageIcon from '@mui/icons-material/Language';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
-const theme = createTheme();
-
 const RegisterPage = () => {
   const { t, i18n } = useTranslation();
   const navigate = useNavigate();
+  const theme = useTheme(); // Usar el tema actual
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [passwordStrength, setPasswordStrength] = useState(0);
+  const [showPasswordStrength, setShowPasswordStrength] = useState(false);
+  const [passwordTimeout, setPasswordTimeout] = useState<number | null>(null);
   const [errors, setErrors] = useState({
     firstName: '',
     lastName: '',
@@ -38,7 +39,6 @@ const RegisterPage = () => {
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Validate firstName and lastName
     if (!data.get('firstName')) {
       newErrors.firstName = t('FIRST_NAME_REQUIRED');
       isValid = false;
@@ -48,13 +48,11 @@ const RegisterPage = () => {
       isValid = false;
     }
 
-    // Validate birthDate
     if (!data.get('birthDate')) {
       newErrors.birthDate = t('BIRTH_DATE_REQUIRED');
       isValid = false;
     }
 
-    // Validate email
     const email = data.get('email') as string;
     const confirmEmail = data.get('confirmEmail') as string;
 
@@ -66,13 +64,11 @@ const RegisterPage = () => {
       isValid = false;
     }
 
-    // Validate email confirmation
     if (confirmEmail !== email) {
       newErrors.confirmEmail = t('EMAILS_DO_NOT_MATCH');
       isValid = false;
     }
 
-    // Validate password
     const password = data.get('password') as string;
     const confirmPassword = data.get('confirmPassword') as string;
 
@@ -84,7 +80,6 @@ const RegisterPage = () => {
       isValid = false;
     }
 
-    // Validate password confirmation
     if (confirmPassword !== password) {
       newErrors.confirmPassword = t('PASSWORDS_DO_NOT_MATCH');
       isValid = false;
@@ -107,6 +102,17 @@ const RegisterPage = () => {
     const password = event.target.value;
     const strength = calculatePasswordStrength(password);
     setPasswordStrength(strength);
+    setShowPasswordStrength(true);
+
+    if (passwordTimeout) {
+      clearTimeout(passwordTimeout);
+    }
+
+    const timeout = setTimeout(() => {
+      setShowPasswordStrength(false);
+    }, 5000);
+
+    setPasswordTimeout(timeout as unknown as number);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -131,19 +137,16 @@ const RegisterPage = () => {
   };
 
   const getPasswordStrengthColor = () => {
-    if (passwordStrength === 100) return 'green';
-    if (passwordStrength >= 75) return 'yellow';
-    return 'red';
+    if (passwordStrength === 100) return theme.palette.success.main;  // Verde
+    if (passwordStrength >= 75) return theme.palette.warning.main;    // Amarillo
+    return theme.palette.error.main;                                   // Rojo
   };
 
-  // Idiomas disponibles
   const availableLanguages = [
     { code: 'en', label: 'English' },
     { code: 'es', label: 'Español' },
-    // Añadir más idiomas si es necesario
   ];
 
-  // Filtrar el idioma actual
   const languagesToShow = availableLanguages.filter(
     (lang) => lang.code !== i18n.language
   );
@@ -162,110 +165,109 @@ const RegisterPage = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            {t('REGISTER')}
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label={t('FIRST_NAME')}
-                  autoFocus
-                  error={!!errors.firstName}
-                  helperText={errors.firstName}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label={t('LAST_NAME')}
-                  name="lastName"
-                  autoComplete="family-name"
-                  error={!!errors.lastName}
-                  helperText={errors.lastName}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="birthDate"
-                  label={t('BIRTH_DATE')}
-                  name="birthDate"
-                  type="date"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  error={!!errors.birthDate}
-                  helperText={errors.birthDate}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label={t('EMAIL_ADDRESS_PLACEHOLDER')}
-                  name="email"
-                  autoComplete="email"
-                  error={!!errors.email}
-                  helperText={errors.email}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="confirmEmail"
-                  label={t('CONFIRM_EMAIL_ADDRESS')}
-                  name="confirmEmail"
-                  autoComplete="email"
-                  error={!!errors.confirmEmail}
-                  helperText={errors.confirmEmail}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label={t('PASSWORD_PLACEHOLDER')}
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  error={!!errors.password}
-                  helperText={errors.password}
-                  onChange={handlePasswordChange}
-                />
-                {/* Barra de fortaleza de contraseña */}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {t('REGISTER')}
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="firstName"
+                required
+                fullWidth
+                id="firstName"
+                label={t('FIRST_NAME')}
+                autoFocus
+                error={!!errors.firstName}
+                helperText={errors.firstName}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label={t('LAST_NAME')}
+                name="lastName"
+                autoComplete="family-name"
+                error={!!errors.lastName}
+                helperText={errors.lastName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="birthDate"
+                label={t('BIRTH_DATE')}
+                name="birthDate"
+                type="date"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                error={!!errors.birthDate}
+                helperText={errors.birthDate}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label={t('EMAIL_ADDRESS_PLACEHOLDER')}
+                name="email"
+                autoComplete="email"
+                error={!!errors.email}
+                helperText={errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="confirmEmail"
+                label={t('CONFIRM_EMAIL_ADDRESS')}
+                name="confirmEmail"
+                autoComplete="email"
+                error={!!errors.confirmEmail}
+                helperText={errors.confirmEmail}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label={t('PASSWORD_PLACEHOLDER')}
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                error={!!errors.password}
+                helperText={errors.password}
+                onChange={handlePasswordChange}
+              />
+              {showPasswordStrength && (
                 <Box sx={{ mt: 2 }}>
                   <Typography variant="body2">
                     {passwordStrength === 100
                       ? t('PASSWORD_STRONG')
                       : passwordStrength >= 75
-                      ? t('PASSWORD_MODERATE')
-                      : t('PASSWORD_WEAK')}
+                        ? t('PASSWORD_MODERATE')
+                        : t('PASSWORD_WEAK')}
                   </Typography>
                   <LinearProgress
                     variant="determinate"
@@ -273,75 +275,75 @@ const RegisterPage = () => {
                     sx={{
                       height: 10,
                       borderRadius: 5,
-                      backgroundColor: '#e0e0e0',
+                      backgroundColor: theme.palette.background.default, // Fondo del tema
                       '& .MuiLinearProgress-bar': {
-                        backgroundColor: getPasswordStrengthColor(),
+                        backgroundColor: getPasswordStrengthColor(), // Color dinámico
                       },
                     }}
                   />
                 </Box>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="confirmPassword"
-                  label={t('CONFIRM_PASSWORD_PLACEHOLDER')}
-                  type="password"
-                  id="confirmPassword"
-                  autoComplete="new-password"
-                  error={!!errors.confirmPassword}
-                  helperText={errors.confirmPassword}
-                />
-              </Grid>
+              )}
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {t('REGISTER')}
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link variant="body2" onClick={onLoginClick}>
-                  {t('ALREADY_HAVE_AN_ACCOUNT')}
-                </Link>
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="confirmPassword"
+                label={t('CONFIRM_PASSWORD_PLACEHOLDER')}
+                type="password"
+                id="confirmPassword"
+                autoComplete="new-password"
+                error={!!errors.confirmPassword}
+                helperText={errors.confirmPassword}
+              />
             </Grid>
-          </Box>
-        </Box>
-
-        {/* Botón de selección de idioma */}
-        <Tooltip title={t('CHANGE_LANGUAGE')} aria-label="change-language">
-          <IconButton
-            color="inherit"
-            aria-label="change-language"
-            onClick={handleLanguageMenuClick}
-            sx={{
-              position: 'fixed',
-              top: 16,
-              right: 16,
-            }}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <LanguageIcon />
-          </IconButton>
-        </Tooltip>
+            {t('REGISTER')}
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link variant="body2" onClick={onLoginClick}>
+                {t('ALREADY_HAVE_AN_ACCOUNT')}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
 
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
+      {/* Botón de selección de idioma */}
+      <Tooltip title={t('CHANGE_LANGUAGE')} aria-label="change-language">
+        <IconButton
+          color="inherit"
+          aria-label="change-language"
+          onClick={handleLanguageMenuClick}
+          sx={{
+            position: 'fixed',
+            top: 16,
+            right: 16,
+          }}
         >
-          {languagesToShow.map((lang) => (
-            <MenuItem key={lang.code} onClick={() => handleLanguageChange(lang.code)}>
-              {lang.label}
-            </MenuItem>
-          ))}
-        </Menu>
-      </Container>
-    </ThemeProvider>
+          <LanguageIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        {languagesToShow.map((lang) => (
+          <MenuItem key={lang.code} onClick={() => handleLanguageChange(lang.code)}>
+            {lang.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Container>
   );
 };
 
