90df3390b7aa9b683130494f40b73a3305a1dc04
Marat Joseph Lanza Meitchouk <80351751+Meitchouk@users.noreply.github.com>
2024-11-07 23:22:15 -0600

    fix: build error solve

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
